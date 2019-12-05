0868d01d0415b57916d903a37d068b81265dee2a
SeDonk <58569201+SeDonk@users.noreply.github.com>
2019-12-05 20:10:12 +0100

    Advent

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Advent challenge");
+		Opdracht1 opdracht1 = new Opdracht1();
+		System.out.println(opdracht1.totalMassFuel());
+	}
+}
